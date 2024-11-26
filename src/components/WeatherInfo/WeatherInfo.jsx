16204bc21fdbe8b591461592b6a513e913024adb
sunnySavlot <sunny.savlot@gmail.com>
2024-11-26 18:08:23 +0530

    icons added for the weather condition

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react"
 import WbSunnyIcon from "@mui/icons-material/WbSunny"
-// import ThunderstormIcon from "@mui/icons-material/Thunderstorm"
-// import CloudIcon from "@mui/icons-material/Cloud"
-
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm"
+import CloudIcon from "@mui/icons-material/Cloud"
+import AcUnitIcon from "@mui/icons-material/AcUnit"
+import { FaCloudRain } from "react-icons/fa"
+import { BsCloudHaze } from "react-icons/bs"
 export default function WeatherInfo({ weatherData }) {
   const [isCelsius, setIsCelsius] = useState(true)
 
+  if (!weatherData.weather || weatherData.weather.length === 0) {
+    return "info Loading..."
+  }
+
   let icon = <WbSunnyIcon />
+
+  if (weatherData.weather[0].main === "Thunderstorm")
+    icon = <ThunderstormIcon />
+  if (weatherData.weather[0].main === "Rain") icon = <FaCloudRain />
+  if (weatherData.weather[0].main === "Snow") icon = <AcUnitIcon />
+  if (weatherData.weather[0].main === "Clouds") icon = <CloudIcon />
+  if (weatherData.weather[0].main === "Haze") icon = <BsCloudHaze />
 
   function handleClick() {
     setIsCelsius(!isCelsius)
-  }
-
-  if (!weatherData.weather || weatherData.weather.length === 0) {
-    return "info Loading..."
   }
 
   return (
@@ -32,7 +41,7 @@ export default function WeatherInfo({ weatherData }) {
         </p>
       </div>
       <div>
-        <p>Weather condition: {weatherData.weather[0].description}</p>
+        <p>Weather condition: {weatherData.weather[0].main}</p>
       </div>
       <div>
         <p>Humidity: {weatherData.main?.humidity}</p>
